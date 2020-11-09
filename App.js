d7233276d56ad1d40a97acf73a2bf3de0d1e96a0
Tilman Reipsch <tilman.reipsch@uninow.de>
2020-11-09 12:39:27 +0100

    feat: add buttons for login and reset

--- App.js
+++ App.js
@@ -6,21 +6,22 @@
  * @flow strict-local
  */
 
-import React,{useRef, useState} from 'react';
-import {SafeAreaView, StatusBar,Text,TouchableOpacity} from 'react-native';
+import React, {useRef, useState} from 'react';
+import {SafeAreaView, StatusBar, Text, TouchableOpacity, View} from 'react-native';
 import AutoLoginWebView from './src/AutoLoginWebView';
 import config from './src/config';
 
-const App= () => {
+const App = () => {
+  const [key, setKey] = useState(1);
   const webView = useRef();
   const [status, setStatus] = useState('idle');
-  const credentials={
-    user:'test',
-    password:'test',
-  }
+  const credentials = {
+    user: 'test',
+    password: 'test',
+  };
   const doLogin = async () => {
     try {
-      setStatus('loading')
+      setStatus('loading');
       await webView.current.doLogin(credentials, 10000);
       setStatus('success');
       return true;
@@ -34,9 +35,35 @@ const App= () => {
   return (
     <>
       <StatusBar barStyle="dark-content" />
-      <SafeAreaView style={{flex:1}}><AutoLoginWebView config={config}
-                                                       ref={webView}/>
-      <TouchableOpacity style={{ height: 50, alignItems: 'center', justifyContent:'center', backgroundColor:'#bfbfbf'}} onPress={doLogin}><Text>Run</Text><Text>{status}</Text></TouchableOpacity></SafeAreaView>
+      <SafeAreaView style={{flex: 1}}>
+        <AutoLoginWebView config={config} ref={webView} key={key} />
+        <View style={{height: 50, flexDirection: 'row'}}>
+          <TouchableOpacity
+            style={{
+              flex: 1,
+              alignItems: 'center',
+              justifyContent: 'center',
+              backgroundColor: '#bfbfbf',
+            }}
+            onPress={doLogin}>
+            <Text>Run</Text>
+            <Text>{status}</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={{
+              flex: 0.2,
+              alignItems: 'center',
+              justifyContent: 'center',
+              backgroundColor: '#bfbfbf',
+            }}
+            onPress={() => {
+              setStatus('idle');
+              setKey(key + 1);
+            }}>
+            <Text>Reset</Text>
+          </TouchableOpacity>
+        </View>
+      </SafeAreaView>
     </>
   );
 };
